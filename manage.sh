7740de800be08b799af812baa8dee613789f6817
Jan-Luca Klees <email@janlucaklees.de>
2023-01-07 20:57:07 +0100

    Added possibility of services defining their own, dynamic env variables

--- manage.sh
+++ manage.sh
@@ -4,6 +4,7 @@
 service_name=$1
 service_root="${PWD}/${service_name}"
 service_compose="${service_root}/docker-compose.yml"
+service_dynamic_env="${service_root}/.env.sh"
 
 # Make sure there actually is some configuration for the given service name.
 if [ ! -f  "${service_compose}" ]; then
@@ -11,10 +12,17 @@ if [ ! -f  "${service_compose}" ]; then
     exit 1
 fi
 
-# Load environment variables
+# Load global environment variables
 set -o allexport
 . ${PWD}/.env
 set +o allexport
+
+# Load dynamic, service specific environment variables
+if [ -f  "${service_dynamic_env}" ]; then
+    set -o allexport
+    . ${service_dynamic_env}
+    set +o allexport
+fi
 
 # Assemble the docker-compose command
 DC="docker compose"
