6fb2880adf724cec484870733f666cc901269078
Jan-Luca Klees <email@janlucaklees.de>
2023-01-07 20:10:57 +0100

    Removed global project name
    I realized that having all different services grouped together makes
    things worse, not better. They are suddenly all in the default network
    together with all other services, unless configured otherwise. I always
    get the message that there are orphaned containers, when starting a part
    of the project. And I realized, that I won't really be able to run
    multiple projects like this one together on the same server.And there
    aren't just any good benefits to justify these inconveniences.
    
    So with this, I revert back to be treating the individual parts of this
    project as that: Individual. They might share config for traefik, or
    watchtower, when they need to, but otherwise things are separated.
    
    I kept the manage.sh as it makes it very convenient to work with this
    setup.

--- manage.sh
+++ manage.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
 # Give the first argument a name for more readable code.
-service_group=$1
-service_group_root="./${service_group}"
-service_group_env="${service_group_root}/.env"
-service_group_compose="${service_group_root}/docker-compose.yml"
+service_name=$1
+service_root="./${service_name}"
+service_compose="${service_root}/docker-compose.yml"
 
-# Make sure there actually is some configuration for the given service group.
-if [ ! -f  "${service_group_compose}" ]; then
-    echo "No service group '${service_group}' found."
+# Make sure there actually is some configuration for the given service name.
+if [ ! -f  "${service_compose}" ]; then
+    echo "No service group '${service_name}' found."
     exit 1
 fi
 
@@ -16,7 +15,7 @@ fi
 . ${PWD}/.env
 
 # Assemble the docker-compose command
-DC="PROJECT_NAME=${PROJECT_NAME} docker compose --project-name ${PROJECT_NAME}"
+DC="docker compose"
 
-cd $service_group_root
+cd $service_root
 eval $DC "${@:2}"
